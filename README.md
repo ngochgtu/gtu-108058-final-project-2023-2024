7d567c41186f825907cf12adef0c0f07333eb8a6
Nikoloz Gochiashvili <nikoloz.gochiashvili@fundbox.com>
2023-10-16 13:21:03 +0400

    Add student CRUD

--- README.md
+++ README.md
@@ -1,17 +1,33 @@
 # gtu-108058-final-project-2023-2024
-Main repository
+Project Name: Sills Verifier
 
 Installation:
     Nodejs
     yarn
 
+Technologies: nestjs, reactjs
+Database: Postgresql
+
 Ide:
     Visual Studio Code
     WebStorm
-    ...
 
 backend:
     yarn yarn start:dev
 
 frontend:
     yarn start
+
+
+Milestones:
+    https://docs.google.com/document/d/107u0xy_Vxg03s7NwIPLJO15QY7RCXZYtrLeVRY8iwOw/edit?usp=sharing
+
+PRD:
+    https://docs.google.com/document/d/1-jbzvmvZNSEYBwaiWB4UX3vkv8VuiDEG556nfnsmzJw/edit?usp=sharing
+
+Research Proposal:
+    https://docs.google.com/document/d/1TnJPSiOFsB9dWTQtgJ9IYUGXd9afT7wwNu61I0zg3b0/edit?usp=sharing
+
+Presentations:
+
+    https://docs.google.com/presentation/d/16axIyazD9XWjisojPlf6WfGgtOW4wiwXug0e87EYNWU/edit?usp=sharing
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './controller/app.controller';
+import { AppService } from './service/app.service';
+import { StudentController } from './controller/student.controller';
+import { StudentSchema } from './schema/student.schema';
+import { StudentService } from './service/student.service';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://skills_verifier:ZyUSGsUUp7tTZ4d6@cluster0.7xtlism.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
+  ],
+  controllers: [AppController, StudentController],
+  providers: [AppService, StudentService],
 })
 export class AppModule {}
