81f0255bba1fa7172bf6ae6f7fa43f03a577a0c3
Nikoloz Gochiashvili <nikoloz.gochiashvili@fundbox.com>
2023-11-03 12:51:41 +0400

    Add header styles

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,24 @@
 import {Col, Container, Row} from "react-bootstrap";
+import styles from "./Header.css"
 
 const Header = () => {
     return <Container className="p-3">
         <Row>
-            <Col>
-                <h1>Skills Verifier</h1>
-            </Col>
+            <div className={styles.container}>
+                <Col>
+                    <h1>Skills Verifier</h1>
+                </Col>
+                <div className={styles.img_container}>image</div>
+                <div className={styles.content}>
+                    <span>erekle</span>
+                    <span>welcome</span>
+                </div>
+                <div className={styles.button_container}>
+                    <button className={styles.button}>log out</button>
+                </div>
+            </div>
         </Row>
     </Container>
-}
+};
 
 export default Header
