4ec7ea3c2af4a08912ccc1488becb1dbd8080875
luka753 <tskhomelidze.l@gtu.ge>
2023-11-16 14:25:48 +0400

    Update User.Schema.ts

--- backend/src/schema/User.Schema.ts
+++ backend/src/schema/User.Schema.ts
@@ -1,17 +1,1 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-@Schema()
-export class User{
-    @Prop()
-    name: string
-    @Prop()
-    email: string
-    @Prop()
-    password: string
-    @Prop()
-    skill: string
-    @Prop()
-    points: number
-}
-
-export const UserSchema = SchemaFactory.createForClass(User);
