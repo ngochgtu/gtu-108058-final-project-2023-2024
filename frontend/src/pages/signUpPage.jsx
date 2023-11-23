3dc173abfbe4fd103387b9153f61db0ab3f7775e
Gela <68652298+LuQ1z@users.noreply.github.com>
2023-11-23 21:46:57 +0400

    Added verification on frontend side and fixed user registration

--- frontend/src/pages/signUpPage.jsx
+++ frontend/src/pages/signUpPage.jsx
@@ -3,28 +3,49 @@ import "../style/signUp.styles.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { BASE_PATH } from "../api/ServerApi";
-// import axios from "axios";
 
 const SignUpPage = () => {
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const [formData, setFormData] = useState({
+		username: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
 
 	const navigate = useNavigate();
 
-	const handleRegister = async () => {
+	const [passwordError, setPasswordError] = useState("");
+
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+		setPasswordError("");
+	};
+
+	const handleRegister = async (e) => {
+		e.preventDefault();
+
+		if (formData.password !== formData.confirmPassword) {
+			setPasswordError("Passwords do not match");
+			return;
+		}
+
 		try {
 			const response = await fetch(`${BASE_PATH}/users/user`, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
 				},
-				body: JSON.stringify({ username ,email, password, }),
-			})
+				body: JSON.stringify({
+					username: formData.username,
+					email: formData.email,
+					password: formData.password,
+				}),
+			});
 
 			if (response.ok) {
 				const data = await response.json();
 				console.log("Registration successful:", data);
+				navigate("/sign-in");
 			} else {
 				console.error("Registration failed");
 			}
@@ -33,18 +54,9 @@ const SignUpPage = () => {
 		}
 	};
 
-	const navigationMethod = () => {
-		navigate("/sign-in");
-	};
-
-	const handleButtonClick = () => {
-		handleRegister();
-		navigationMethod();
-	};
-
 	return (
 		<div>
-			<form>
+			<form onSubmit={handleRegister}>
 				<div className="signUp-container">
 					<h3 className="signUp-title">Sign Up</h3>
 					<div className="signUp--input__container">
@@ -53,8 +65,9 @@ const SignUpPage = () => {
 							type="text"
 							className="signUp-input"
 							placeholder="Username"
-							value={username}
-							onChange={(e) => setUsername(e.target.value)}
+							name="username"
+							value={formData.username}
+							onChange={handleChange}
 						/>
 					</div>
 					<div className="signUp--input__container">
@@ -63,8 +76,9 @@ const SignUpPage = () => {
 							type="email"
 							className="signUp-input"
 							placeholder="Enter email"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
+							name="email"
+							value={formData.email}
+							onChange={handleChange}
 						/>
 					</div>
 					<div className="signUp--input__container">
@@ -73,8 +87,9 @@ const SignUpPage = () => {
 							type="password"
 							className="signUp-input"
 							placeholder="Password"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
+							name="password"
+							value={formData.password}
+							onChange={handleChange}
 						/>
 					</div>
 					<div className="signUp--input__container">
@@ -83,19 +98,18 @@ const SignUpPage = () => {
 							type="password"
 							className="signUp-input"
 							placeholder="Password"
+							name="confirmPassword"
+							value={formData.confirmPassword}
+							onChange={handleChange}
 						/>
 					</div>
 					<div className="signUp--button_container">
-						<button
-							type="submit"
-							className="signUp-button"
-							onClick={handleButtonClick}
-						>
+						<button type="submit" className="signUp-button">
 							Sign Up
 						</button>
 					</div>
 					<p className="forgot-password text-right">
-						Already registered <Link to="/sign-in">sign in</Link>
+						Already registered? <Link to="/sign-in">sign in</Link>
 					</p>
 				</div>
 			</form>
