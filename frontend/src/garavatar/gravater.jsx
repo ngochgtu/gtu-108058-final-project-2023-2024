3e0abe91b5e6c31dacebc2387dcb002bbfc06e58
ereklemanjavidze <ereklemanjavidze@gmail.com>
2023-11-26 22:29:13 +0400

    header and profile update

--- frontend/src/garavatar/gravater.jsx
+++ frontend/src/garavatar/gravater.jsx
@@ -2,7 +2,6 @@ import md5 from "md5";
 
 export const gravatarUrl = (email) => {
   if (!email) return "";
-
   const emailHash = md5(email);
 
   return `https://www.gravatar.com/avatar/${emailHash}?s=200`;
