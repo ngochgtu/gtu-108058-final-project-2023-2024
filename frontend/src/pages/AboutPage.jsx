b04155c158c3c534fab043544f736b6851aa6b67
Zone-OTP <zezva.mebagishvili5@mail.ru>
2023-11-30 12:35:30 +0400

    modified:   backend/yarn.lock 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/pages/AboutPage.jsx 	modified:   frontend/src/style/About.module.css 	modified:   frontend/yarn.lock

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import styles from "../style/About.module.css";
 
+import { Link } from "react-router-dom";
+
 const AboutPage = () => {
   return (
     <Container>
-      <div className={styles.text}>About Page</div>
+      <div style={{ textAlign: "center" }} className={styles.text}>
+        About Page
+        <p>This app was made as a final project of GTU group 108058</p>
+        <p>
+          he app is a skills verifier that utilizes AI to generate questions and
+          subsequently employs the same AI to assess your skills.
+        </p>
+        <p>
+          To better understand it,
+          <Link className={`${styles.SginUp}`} to="/sign-up">
+            sign up
+          </Link>
+          , use the application on your own, and assess your skills using the
+          available options
+        </p>
+      </div>
     </Container>
   );
 };
